f8e80745fa1e1fd30edc1c9e542c3271568466e1
Bernardo Afonso <54000972+be-rush1@users.noreply.github.com>
2024-09-15 15:48:47 -0300

    Update corta_dados.py

--- corta_dados.py
+++ corta_dados.py
@@ -3,9 +3,10 @@ import xarray as xr
 import geopandas
 import os
 from shapely.geometry import mapping
-arquivo = str(os.environ["ano_arquivo"])
-data = xr.open_dataset('chirps-v2.0.'+ arquivo + '.monthly.nc')
+arquivo = str(os.environ["ANO_ARQUIVO"])
+path = str(os.environ["WORKSPACE"])
+data = xr.open_dataset(path + '/chirps-v2.0.'+ arquivo + '.monthly.nc')
 data.rio.set_spatial_dims(x_dim="latitude", y_dim="longitude", inplace=True)
 data.rio.write_crs("epsg:4326", inplace=True)
-sudeste = geopandas.read_file('teste/BR_regiao_sudeste_2022/BR_regiao_sudeste_2022.sh', crs="epsg:4326")
+sudeste = geopandas.read_file(path + '/BR_regiao_sudeste_2022/BR_regiao_sudeste_2022.sh', crs="epsg:4326")
 clipped = data.rio.clip(sudeste.geometry.apply(mapping), sudeste.crs, drop=True)
